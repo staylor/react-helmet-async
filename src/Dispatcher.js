86b3220ccbb90dad61e6db8afe01e94249a15dab
Lukas Degener <l.degener@tarent.de>
2018-07-19 14:12:24 +0200

    FIXES #18: replace Object.assign with spread op

--- src/Dispatcher.js
+++ src/Dispatcher.js
@@ -36,7 +36,7 @@ export default class Dispatcher extends Component {
     let serverState = null;
     const state = reducePropsToState(
       helmetInstances.get().map(instance => {
-        const props = Object.assign({}, instance.props);
+        const props = {...instance.props};
         delete props.context;
         return props;
       })
