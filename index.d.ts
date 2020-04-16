f9587b8707d330b8bd9625842a23e1293e93bc1e
Pikachews <Pikachews@users.noreply.github.com>
2020-04-15 23:59:55 -0700

    Fix HelmetDatum.toComponent type

--- index.d.ts
+++ index.d.ts
@@ -59,7 +59,7 @@ declare module 'react-helmet-async' {
 
   export interface HelmetDatum {
     toString(): string;
-    toComponent(): React.Component<any>;
+    toComponent(): Array<React.Component<any>>;
   }
 
   export interface HelmetHTMLBodyDatum {
