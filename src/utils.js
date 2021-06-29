94e46f783290f2f8a0332b8f19ecd703e5a6f7c8
Justin Heideman <justin.heideman@nytimes.com>
2021-06-29 16:07:10 -0500

    fix: apply scotts changes

--- src/utils.js
+++ src/utils.js
@@ -21,11 +21,6 @@ const getInnermostProperty = (propsList, property) => {
   return null;
 };
 
-const getAnyTrueFromPropsList = (propsList, checkedTag) =>
-  propsList.reduce((accumulator, currentPropList) =>
-    currentPropList[checkedTag] ? true : accumulator
-  );
-
 const getTitleFromPropsList = propsList => {
   let innermostTitle = getInnermostProperty(propsList, TAG_NAMES.TITLE);
   const innermostTemplate = getInnermostProperty(propsList, HELMET_PROPS.TITLE_TEMPLATE);
@@ -176,23 +171,64 @@ const getTagsFromPropsList = (tagName, primaryAttributes, propsList) => {
     .reverse();
 };
 
-// helper to inspect for matching props on components
-export const checkIfPropsMatch = (props, toMatch) => {
-  const pairs = Object.entries(props);
-  for (let i = 0; i < pairs.length; i += 1) {
-    const [propName, propVal] = pairs[i];
-    if (toMatch[propName]) {
-      // e.g. if rel exists in the list of allowed props
-      const propWeAreCheckingOut = toMatch[propName]; // e.g. [amphtml, alternate, etc]
-      if (propWeAreCheckingOut.includes(propVal)) {
-        return true;
-      }
+const getAnyTrueFromPropsList = (propsList, checkedTag) =>
+  propsList.reduce((accumulator, currentPropList) =>
+    currentPropList[checkedTag] ? true : accumulator
+  );
+
+const reducePropsToState = propsList => ({
+  baseTag: getBaseTagFromPropsList([TAG_PROPERTIES.HREF], propsList),
+  bodyAttributes: getAttributesFromPropsList(ATTRIBUTE_NAMES.BODY, propsList),
+  defer: getInnermostProperty(propsList, HELMET_PROPS.DEFER),
+  encode: getInnermostProperty(propsList, HELMET_PROPS.ENCODE_SPECIAL_CHARACTERS),
+  htmlAttributes: getAttributesFromPropsList(ATTRIBUTE_NAMES.HTML, propsList),
+  linkTags: getTagsFromPropsList(
+    TAG_NAMES.LINK,
+    [TAG_PROPERTIES.REL, TAG_PROPERTIES.HREF],
+    propsList
+  ),
+  metaTags: getTagsFromPropsList(
+    TAG_NAMES.META,
+    [
+      TAG_PROPERTIES.NAME,
+      TAG_PROPERTIES.CHARSET,
+      TAG_PROPERTIES.HTTPEQUIV,
+      TAG_PROPERTIES.PROPERTY,
+      TAG_PROPERTIES.ITEM_PROP,
+    ],
+    propsList
+  ),
+  noscriptTags: getTagsFromPropsList(TAG_NAMES.NOSCRIPT, [TAG_PROPERTIES.INNER_HTML], propsList),
+  onChangeClientState: getOnChangeClientState(propsList),
+  scriptTags: getTagsFromPropsList(
+    TAG_NAMES.SCRIPT,
+    [TAG_PROPERTIES.SRC, TAG_PROPERTIES.INNER_HTML],
+    propsList
+  ),
+  styleTags: getTagsFromPropsList(TAG_NAMES.STYLE, [TAG_PROPERTIES.CSS_TEXT], propsList),
+  title: getTitleFromPropsList(propsList),
+  titleAttributes: getAttributesFromPropsList(ATTRIBUTE_NAMES.TITLE, propsList),
+  prioritizeSeoTags:
+    propsList.prioritizeSeoTags &&
+    getAnyTrueFromPropsList(propsList, HELMET_PROPS.PRIORITIZE_SEO_TAGS),
+});
+
+export const flattenArray = possibleArray =>
+  Array.isArray(possibleArray) ? possibleArray.join('') : possibleArray;
+
+export { reducePropsToState };
+
+const checkIfPropsMatch = (props, toMatch) => {
+  const keys = Object.keys(props);
+  for (let i = 0; i < keys.length; i += 1) {
+    // e.g. if rel exists in the list of allowed props [amphtml, alternate, etc]
+    if (toMatch[keys[i]] && toMatch[keys[i]].includes(props[keys[i]])) {
+      return true;
     }
   }
   return false;
 };
 
-// re-usable fn to prioritize tags by matching props
 export const prioritizer = (elementsList, propsToMatch) => {
   if (Array.isArray(elementsList)) {
     return elementsList.reduce(
@@ -209,45 +245,3 @@ export const prioritizer = (elementsList, propsToMatch) => {
   }
   return { default: elementsList };
 };
-
-const reducePropsToState = propsList => {
-  return {
-    baseTag: getBaseTagFromPropsList([TAG_PROPERTIES.HREF], propsList),
-    bodyAttributes: getAttributesFromPropsList(ATTRIBUTE_NAMES.BODY, propsList),
-    defer: getInnermostProperty(propsList, HELMET_PROPS.DEFER),
-    encode: getInnermostProperty(propsList, HELMET_PROPS.ENCODE_SPECIAL_CHARACTERS),
-    htmlAttributes: getAttributesFromPropsList(ATTRIBUTE_NAMES.HTML, propsList),
-    linkTags: getTagsFromPropsList(
-      TAG_NAMES.LINK,
-      [TAG_PROPERTIES.REL, TAG_PROPERTIES.HREF],
-      propsList
-    ),
-    metaTags: getTagsFromPropsList(
-      TAG_NAMES.META,
-      [
-        TAG_PROPERTIES.NAME,
-        TAG_PROPERTIES.CHARSET,
-        TAG_PROPERTIES.HTTPEQUIV,
-        TAG_PROPERTIES.PROPERTY,
-        TAG_PROPERTIES.ITEM_PROP,
-      ],
-      propsList
-    ),
-    noscriptTags: getTagsFromPropsList(TAG_NAMES.NOSCRIPT, [TAG_PROPERTIES.INNER_HTML], propsList),
-    onChangeClientState: getOnChangeClientState(propsList),
-    scriptTags: getTagsFromPropsList(
-      TAG_NAMES.SCRIPT,
-      [TAG_PROPERTIES.SRC, TAG_PROPERTIES.INNER_HTML],
-      propsList
-    ),
-    styleTags: getTagsFromPropsList(TAG_NAMES.STYLE, [TAG_PROPERTIES.CSS_TEXT], propsList),
-    title: getTitleFromPropsList(propsList),
-    titleAttributes: getAttributesFromPropsList(ATTRIBUTE_NAMES.TITLE, propsList),
-    prioritizeSeoTags: getAnyTrueFromPropsList(propsList, HELMET_PROPS.PRIORITIZE_SEO_TAGS),
-  };
-};
-
-export const flattenArray = possibleArray =>
-  Array.isArray(possibleArray) ? possibleArray.join('') : possibleArray;
-
-export { reducePropsToState };
