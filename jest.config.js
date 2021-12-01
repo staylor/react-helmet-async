bcce92e4e3b5e10c131fc7b4fe3824d494564590
Scott Taylor <scott.taylor@shopify.com>
2021-12-01 16:37:00 -0500

    fix: ensure that tests run

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,4 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/src/', '/lib/', '/es/'],
 };
